701405483eb2ec752286658896c4278b54d381ec
chrisIyer <hellohotbox420@gmail.com>
2023-03-19 15:23:26 -0400

    theme changed to dark and practiced with somr lodash . import { customSort,customStyles, conditionalRowStyles, resetPaginationToggle, selectableRows } from 'lodash'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import './App.css';
 //import Item from './components/Item';
+import React from 'react';
 import data from './data';
 import columns from './columns';
 import DataTable from 'react-data-table-component';
-
-// const cards = cardsArr.map((ele, idx) => {
-// 	return (
-// 		<Item
-// 			{...ele}
-// 			key={idx}
-// 		/>
-// 	);
-// });
+import { customSort,customStyles, conditionalRowStyles, resetPaginationToggle, selectableRows } from 'lodash'
 
 export default function App() {
+	const [selectedRows, setSelectedRows] = React.useState(false);
+	const [toggledClearRows, setToggleClearRows] = React.useState(false);
+   
+	const handleChange = ({ selectedRows }) => {
+	  setSelectedRows(selectedRows);
+	};
+   
+	// Toggle the state so React Data Table changes to clearSelectedRows are triggered
+	const handleClearRows = () => {
+	  setToggleClearRows(!toggledClearRows);
+	}
+	
+	
+
+
 	return (
 		<div className="App">
+			 <button onClick={handleClearRows}>
+        Clear Selected Rows
+      </button>
 			<DataTable
-			columns={columns}
-			data={data} />
+				columns={columns}
+				data={data}
+				pagination
+				selectableRows
+				customStyles={customStyles}
+				conditionalRowStyles={conditionalRowStyles}
+				paginationResetDefaultPage={resetPaginationToggle} // optionally, a hook to reset pagination to page 1
+
+				dense
+				theme="dark" 
+				onSelectedRowsChange={handleChange}
+				sortFunction={customSort}
+        clearSelectedRows={toggledClearRows}
+
+			/>
 		</div>
 	);
 }
