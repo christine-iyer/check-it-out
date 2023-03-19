0d7813e0bea75572f1d839b479c15f3786e47926
chrisIyer <hellohotbox420@gmail.com>
2023-03-19 15:26:45 -0400

    modified a tad.

--- src/App.js
+++ src/App.js
@@ -4,43 +4,37 @@ import React from 'react';
 import data from './data';
 import columns from './columns';
 import DataTable from 'react-data-table-component';
-import { customSort,customStyles, conditionalRowStyles, resetPaginationToggle, selectableRows } from 'lodash'
+import { customSort, customStyles, conditionalRowStyles, resetPaginationToggle, selectableRows } from 'lodash'
 
 export default function App() {
 	const [selectedRows, setSelectedRows] = React.useState(false);
 	const [toggledClearRows, setToggleClearRows] = React.useState(false);
-   
+
 	const handleChange = ({ selectedRows }) => {
-	  setSelectedRows(selectedRows);
+		setSelectedRows(selectedRows);
 	};
-   
-	// Toggle the state so React Data Table changes to clearSelectedRows are triggered
 	const handleClearRows = () => {
-	  setToggleClearRows(!toggledClearRows);
+		setToggleClearRows(!toggledClearRows);
 	}
-	
-	
-
-
 	return (
 		<div className="App">
-			 <button onClick={handleClearRows}>
-        Clear Selected Rows
-      </button>
+			<button onClick={handleClearRows}>
+				Clear Selected Rows
+			</button>
 			<DataTable
 				columns={columns}
 				data={data}
 				pagination
-				selectableRows
+				selectableRows={selectedRows}
 				customStyles={customStyles}
 				conditionalRowStyles={conditionalRowStyles}
 				paginationResetDefaultPage={resetPaginationToggle} // optionally, a hook to reset pagination to page 1
 
 				dense
-				theme="dark" 
+				theme="dark"
 				onSelectedRowsChange={handleChange}
 				sortFunction={customSort}
-        clearSelectedRows={toggledClearRows}
+				clearSelectedRows={toggledClearRows}
 
 			/>
 		</div>
