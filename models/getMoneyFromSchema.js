7b6557fdef5e2cbbfca930e158da535788caf4ea
chrisIyer <hellohotbox420@gmail.com>
2023-03-19 16:44:07 -0400

    added models routes controllers for users and payments and sales.

---
+++ models/getMoneyFromSchema.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose')
+
+const getMoneyFromSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  itemPurchased: { type: String, required: true },
+  quantityPurchased: { type: Number, required: true },
+  unitCost:  { type: Number, required: true },
+  tax:  { type: Number, required: true },
+  saleType:  { type: Number, required: true },//categoy
+  accountNumber:  { type: Number, required: true },
+  class:{ type: String, required: true },
+
+  category: {type: String},
+  imageUrl: { type: String, required: true }
+}, {
+  timestamps: true
+})
+
+module.exports = model('GetMoneyFrom', getMoneyFromSchema)
