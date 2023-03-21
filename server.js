0d532f0f347cbaf0d79abb345d92d3ca386d7715
chrisIyer <hellohotbox420@gmail.com>
2023-03-21 19:21:11 -0400

    done.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.use('/api/users', require('./routes/api/users'))
-app.use('/api/blogs', require('./routes/api/blogs'))
+app.use('/api/blogs', require('./routes/api/api/payMoneyTos'))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
--- src/components/Sale.js
+++ src/components/Sale.js
@@ -3,63 +3,92 @@ import Form from 'react-bootstrap/Form';
 
 
 
-export default function CreateBlog ({
-  createBlog,
-  blog,
+export default function CreateTransaction ({
+  createTransaction,
+  transaction,
   handleChange
 }
 ) {
   return (
-    <div className={styles.CreateBlog}>
-      <h2 className={styles.NewBlog}>Create A Blog</h2>
+    <div className={styles.CreateTransaction}>
+      <h2 className={styles.NewTransaction}>Create A Transaction</h2>
       <div className={styles.container}>
         <Form 
-          className={styles.F}
+          className={styles.Form}
           onSubmit={(e) => {
             e.preventDefault()
-            createBlog()
+            createTransaction()
           }}
         >
           
-          <Form.Label>Title
+          <Form.Label>First Name
             <input 
-            className={styles.title}
+            className={styles.firstName}
             type='text' 
-            value={blog.title} 
-            name='title' 
+            value={transaction.firstName} 
+            name='firstName' 
             onChange={handleChange} 
-            placeholder='Title' 
+            placeholder='First Name' 
             />
           </Form.Label>
 
-          <Form.Label>Url
-            <input className={styles.url}
+          <Form.Label>Last Name
+            <input className={styles.lastName}
             type='text' 
-            value={blog.url} 
-            name='url' 
+            value={transaction.lastName} 
+            name='lastName' 
             onChange={handleChange} 
-            placeholder='URL' />
+            placeholder='Last Name' />
           </Form.Label>
 
-          <Form.Label>Pick a category:
+          <Form.Label>Purchase or Sale:
             <Form.Select 
-            
-            options={blog.category} 
-            value={blog.category} 
+            options={transaction.transactionType} 
+            value={transaction.transactionType} 
+            onChange={handleChange} 
+            placeholder='transactionType'
+            name="transactionType">
+              <option value="Sale">Sale</option>
+              <option  value="Purchase">Purchase</option>
+            </Form.Select>
+            </Form.Label>
+
+            <Form.Label>Cash/Barter/Credit
+            <input className={styles.cash}
+            type='boolean' 
+            value={transaction.cash} 
+            name='cash' 
+            onChange={handleChange} 
+            placeholder='Check if Barter' />
+          </Form.Label>
+
+          <Form.Label>Item Purchased:
+            <Form.Select 
+            options={transaction.item} 
+            value={transaction.item} 
             onChange={handleChange} 
             placeholder='Category'
-            name="category">
-              <option value="Family">Family</option>
-              <option  value="Friends">Friends</option>
-              <option  value="Work">Work</option>
-              <option  value="Code">Code</option>
-              <option  value="Misc">Misc</option>
+            name="item">
+              <option value="Item 1">Item 1</option>
+              <option  value="Item 2">Item 2</option>
+            </Form.Select>
+            </Form.Label>
+
+            <Form.Label>Item Purchased:
+            <Form.Select 
+            options={transaction.item} 
+            value={transaction.item} 
+            onChange={handleChange} 
+            placeholder='Category'
+            name="item">
+              <option value="Item 1">Item 1</option>
+              <option  value="Item 2">Item 2</option>
             </Form.Select>
             </Form.Label>
 
           <Button className={styles.button} 
           type='submit' 
-          value='Create Blog' />
+          value='Create Transaction' />
 
         </Form>
       </div>
